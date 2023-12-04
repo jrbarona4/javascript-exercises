19c38ce64c6271015643a170d736bf91f6ebd9e8
jrbarona4 <jeromebarona1@gmail.com>
2023-12-04 10:16:40 +0800

    Do repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else {
+    for (i = 0; i <= num; i++){
+        return string.repeat(num);
+        }
+    }
 };
-
 // Do not edit below this line
 module.exports = repeatString;
