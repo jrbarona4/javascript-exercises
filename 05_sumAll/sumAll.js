a8f2b6c4e63ad75d6d609241cc984388e51944ff
jrbarona4 <jeromebarona1@gmail.com>
2024-01-01 21:29:43 +0800

    Do sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let finalSum = 0;
+    if (firstNum < 0 || secondNum < 0){
+    return 'ERROR';
+    } else if (firstNum >= secondNum){ 
+    for (i = secondNum; i <= firstNum; i++){
+        finalSum = (i * (secondNum + firstNum)) / 2;
+    }
+    return finalSum;
+    } if (firstNum <= secondNum){
+       for (i = firstNum; i <= secondNum; i++){
+        finalSum = (i * (firstNum + secondNum)) / 2;
+    }
+       return finalSum;
+   }
 };
 
 // Do not edit below this line
