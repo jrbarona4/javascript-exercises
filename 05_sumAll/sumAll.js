1c85738f36987a4e5deb90c0a671f76a548ad4e9
jrbarona4 <jeromebarona1@gmail.com>
2024-01-02 18:04:07 +0800

    Do sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,18 +2,21 @@ const sumAll = function(firstNum, secondNum) {
     let finalSum = 0;
     if (firstNum < 0 || secondNum < 0){
     return 'ERROR';
+    } else if (Number.isInteger(firstNum) === false || Number.isInteger(secondNum) === false){
+    return 'ERROR';
     } else if (firstNum >= secondNum){ 
     for (i = secondNum; i <= firstNum; i++){
         finalSum = (i * (secondNum + firstNum)) / 2;
     }
     return finalSum;
-    } if (firstNum <= secondNum){
+    } else if (firstNum <= secondNum){
        for (i = firstNum; i <= secondNum; i++){
         finalSum = (i * (firstNum + secondNum)) / 2;
     }
        return finalSum;
-   }
-};
-
+    } else {
+    return 'ERROR';
+    }
+}
 // Do not edit below this line
 module.exports = sumAll;
